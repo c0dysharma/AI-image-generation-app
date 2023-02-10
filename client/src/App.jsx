827cb429a35a072e8daa81d2035c43cc1b7d0c19
Kuldeep <c0dysharma01@gmail.com>
2023-02-10 19:07:17 +0530

    Moved LoginSignup as pop up window

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,29 +6,41 @@ import { Home, CreatePost, LoginSignup } from './pages';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const dismissPopup = () => setShowPopup((p) => !p);
+
   return (
     <BrowserRouter>
+      {/* Login popup  */}
+      <LoginSignup trigger={showPopup} onBGclick={dismissPopup} />
+
       {/* Nav bar  */}
       <header className="w-full flex justify-between items-center bg-white px-4 py-4 border-b border-b-[#e6ebf4] sm:px-8">
         <Link to="/">
           <img src={logo} alt="Dalle-E logo" className="w-28 object-contain" />
         </Link>
-        <Link
-          to="/login"
-          className="font-inter font-medium bg-purple text-white px-4 py-2 rounded-md">
-          {loggedIn ? 'Create' : 'Login / Signup'}
-        </Link>
+        {loggedIn ? (
+          <Link
+            to="/createPost"
+            className="font-inter font-medium bg-purple text-white px-4 py-2 rounded-md">
+            Create
+          </Link>
+        ) : (
+          <button
+            type="button"
+            onClick={dismissPopup}
+            className="font-inter font-medium bg-purple text-white px-4 py-2 rounded-md">
+            Log in
+          </button>
+        )}
       </header>
 
       {/* Main section  */}
       <main className="w-full min-h-[calc(100vh-73px)] bg-paleWhite px-4 py-8 sm:p-8">
         <Routes>
           <Route path="/" element={<Home />} />
-          {loggedIn ? (
-            <Route path="/createPost" element={<CreatePost />} />
-          ) : (
-            <Route path="/login" element={<LoginSignup />} />
-          )}
+          <Route path="/createPost" element={<CreatePost />} />
         </Routes>
       </main>
     </BrowserRouter>
--- client/src/pages/LoginSignup.jsx
+++ client/src/pages/LoginSignup.jsx
@@ -1,9 +1,122 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FormField, CheckBox } from '../components';
+import { RxCross2 } from 'react-icons/all';
+
+import { FormField, CheckBox, Loader } from '../components';
 import { google } from '../assets';
 
-const LoginSignup = () => {
+const LeftPane = ({ loading, handleChange, handleSubmit, handleGoogleLogin, form }) => {
+  return (
+    <div className="order-1 lg:-order-1 lg:p-12">
+      <h2 className="text-xl font-medium mb-4 text-center lg:text-left md:text-3xl lg:text-4xl">
+        Welcome back
+      </h2>
+      <p className="text-muted text-xs mb-10 text-center lg:text-left md:text-sm">
+        Welcome back! Please enter your details.
+      </p>
+
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+        <FormField
+          labelName="Email"
+          type="text"
+          name="email"
+          placeholder="Enter your email"
+          value={form.email}
+          handleChange={handleChange}
+        />
+
+        <FormField
+          labelName="Password"
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          value={form.password}
+          handleChange={handleChange}
+        />
+
+        <div className="mb-5 flex justify-between">
+          <CheckBox
+            label="Remember for 30 days"
+            name="rememberMe"
+            value={form.rememberMe}
+            handleChange={handleChange}
+          />
+          <Link
+            to="/forgotPassword"
+            className="text-purple font-medium text-xs md:text-sm lg:text-base">
+            Forgot Password
+          </Link>
+        </div>
+      </form>
+
+      <button
+        type="submit"
+        className="font-medium block w-full rounded-lg bg-purple py-3 text-white mb-3 md:mb-3 lg:mb-5">
+        {loading ? 'Signing in...' : 'Sign in'}
+      </button>
+
+      <button
+        type="button"
+        onClick={handleGoogleLogin}
+        className="font-medium w-full rounded-lg bg-white py-3 border-2 mb-3 md:mb-3 lg:mb-5 flex items-center justify-center">
+        <img src={google} className="mr-3 w-6 h-6" alt="Google logo" />
+        Sign in with Google
+      </button>
+
+      <p className="text-muted text-center text-sm">
+        Don&apos;t have an account?{' '}
+        <Link className="text-purple" to="/signup">
+          Sign up
+        </Link>{' '}
+      </p>
+    </div>
+  );
+};
+
+const RightPane = () => {
+  return (
+    <div className="relative flex flex-col justify-center items-center mb-14 lg:bg-gray-100 lg:mb-0">
+      <div className=" w-16 h-16 bg-purple rounded-full lg:w-48 lg:h-48" />
+      <div className="absolute w-32 h-20 backdrop-blur-lg top-1/2 lg:w-64 lg:h-52" />
+    </div>
+  );
+};
+
+const InnerWindow = ({
+  loading,
+  handleChange,
+  handleSubmit,
+  handleGoogleLogin,
+  form,
+  onBGclick
+}) => {
+  return (
+    <>
+      {/* Cross sign  */}
+      <RxCross2
+        className="absolute right-10 top-8 w-8 h-8 cursor-pointer z-20"
+        onClick={onBGclick}
+      />
+
+      {/* Left side form pane  */}
+      <LeftPane
+        loading={loading}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        handleGoogleLogin={handleGoogleLogin}
+        form={form}
+      />
+
+      {/* Right side graphics pane  */}
+      <RightPane />
+    </>
+  );
+};
+
+const LoginSignup = ({ trigger, onBGclick }) => {
   const [loading, setLoading] = useState(false);
   const [form, setForm] = useState({
     email: '',
@@ -21,74 +134,28 @@ const LoginSignup = () => {
   const handleGoogleLogin = () => {};
 
   return (
-    <section className="text-[#2223228] max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2">
-      {/* Left side form pane  */}
-      <div className="p-10 order-1 lg:-order-1">
-        <h2 className="text-4xl font-medium mb-4 text-center lg:text-left">Welcome back</h2>
-        <p className="text-muted text-sm mb-10 text-center lg:text-left">
-          Welcome back! Please enter your details.
-        </p>
-
-        <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
-          <FormField
-            labelName="Email"
-            type="text"
-            name="email"
-            placeholder="Enter your email"
-            value={form.email}
-            handleChange={handleChange}
-          />
-
-          <FormField
-            labelName="Password"
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={form.password}
-            handleChange={handleChange}
-          />
-
-          <div className="mb-5 flex justify-between">
-            <CheckBox
-              label="Remember for 30 days"
-              name="rememberMe"
-              value={form.rememberMe}
+    trigger && (
+      <div
+        className="w-screen h-screen flex items-center justify-center bg-[#0d0d0dac] z-10 overflow-y-scroll fixed"
+        onClick={onBGclick}>
+        {loading ? (
+          <Loader />
+        ) : (
+          <section
+            className="text-[#2223228] overflow-hidden bg-white py-8 px-4 md:px-8 md:py-16 md:rounded-lg grid grid-cols-1 lg:grid-cols-2 lg:py-0 lg:px-0 w-full h-full max-w-5xl md:mx-20 md:h-auto relative"
+            onClick={(e) => e.stopPropagation()}>
+            <InnerWindow
+              loading={loading}
               handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              handleGoogleLogin={handleGoogleLogin}
+              form={form}
+              onBGclick={onBGclick}
             />
-            <Link to="/forgotPassword" className="text-purple font-medium">
-              Forgot Password
-            </Link>
-          </div>
-        </form>
-
-        <button
-          type="submit"
-          className="font-medium block w-full rounded-lg bg-purple py-3 text-white mb-5">
-          {loading ? 'Signing in...' : 'Sign in'}
-        </button>
-
-        <button
-          type="button"
-          onClick={handleGoogleLogin}
-          className="font-medium w-full rounded-lg bg-white py-3 border-2 mb-5 flex items-center justify-center">
-          <img src={google} className="mr-3 w-8 h-8" alt="Google logo" />
-          Sign in with Google
-        </button>
-
-        <p className="text-muted text-center text-sm">
-          Don&apos;t have an account?{' '}
-          <Link className="text-purple" to="/signup">
-            Sign up
-          </Link>{' '}
-        </p>
+          </section>
+        )}
       </div>
-
-      {/* Right side graphics pane  */}
-      <div className="relative flex flex-col justify-center items-center mb-8 lg:bg-gray-100 lg:mb-0">
-        <div className=" w-20 h-20 bg-purple rounded-full lg:w-48 lg:h-48" />
-        <div className="absolute w-36 h-24 backdrop-blur-lg top-1/2 lg:w-64 lg:h-52" />
-      </div>
-    </section>
+    )
   );
 };
 
